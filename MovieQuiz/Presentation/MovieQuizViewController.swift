3c838e2eceeb2a9f62a5b8a7a07ede6f3d49881d
UnwraperMan <unwraperman@gmail.com>
2022-12-20 13:13:11 +0300

    The start screen is laid out. Buttons, question label, image, question counter connected to view controller.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,9 +2,77 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
+    
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var yesButton: UIButton!
+    
+    @IBAction func noButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func yesButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    struct QuizResultViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+    
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+    
+    let questions: [QuizQuestion] = [
+    QuizQuestion(image: "The Godfather",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Dark Knight",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Kill Bill",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Avengers",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Deadpool",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Green Knight",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Old",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false),
+    QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false),
+    QuizQuestion(image: "Tesla",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false),
+    QuizQuestion(image: "Vivarium",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false)
+    ]
 }
 
 /*
