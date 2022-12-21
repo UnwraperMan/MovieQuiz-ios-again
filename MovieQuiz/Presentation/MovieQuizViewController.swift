a547282365b7ccce19025292b409fd76aa067a23
UnwraperMan <unwraperman@gmail.com>
2022-12-21 05:18:16 +0300

    The logic of the transition to the new issue, as well as a pop -up window(alert), is perfectly implemented.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -14,20 +14,25 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet weak var yesButton: UIButton!
     
     @IBAction func noButton(_ sender: UIButton) {
-        let currentQuestion = questions[currentQuestionIndex]
-        let givenAnswer = true
+        noButton.isEnabled = false
         
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer )
-    }
-    
-    @IBAction func yesButton(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
         
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
-    var currentQuestionIndex: Int = 0
+    @IBAction func yesButton(_ sender: UIButton) {
+        yesButton.isEnabled = false
+        
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    private var currentQuestionIndex: Int = 0
+    private var corectAnswers: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,17 +42,34 @@ final class MovieQuizViewController: UIViewController {
         }
         let viewModel = convert(model: firstQuestion)
         show(quiz: viewModel)
-        
-//        show(quiz: convert(model: questions[currentQuestionIndex]))
     }
     
-    func show(quiz step: QuizStepViewModel) {
+    private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
+        
+        noButton.isEnabled = true
+        yesButton.isEnabled = true
     }
     
-    func convert(model: QuizQuestion) -> QuizStepViewModel {
+    private func show(quiz result: QuizResultViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) {_ in
+            self.currentQuestionIndex = 0
+            self.corectAnswers = 0
+            self.show(quiz: self.convert(model: self.questions[self.currentQuestionIndex]))
+        }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(image: UIImage(named: model.image) ?? UIImage(),
                                  question: model.text,
                                  questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
@@ -55,8 +77,29 @@ final class MovieQuizViewController: UIViewController {
     
     private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
-            currentQuestionIndex += 1
+            corectAnswers += 1
         }
+        imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 20
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuesionOrResult()
+            self.imageView.layer.borderColor = UIColor.clear.cgColor
+        }
+    }
+    
+    private func showNextQuesionOrResult() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(corectAnswers) из \(questions.count)"
+            let viewModel = QuizResultViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть еще?")
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1
+            show(quiz: convert(model: questions[currentQuestionIndex]))}
     }
     
     struct QuizStepViewModel {
